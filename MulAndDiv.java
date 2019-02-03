f7a03288424cc1987b647f50e2bf737222dd8e60
subayadhav <suba10499@gmail.com>
2019-02-03 21:43:05 +0530

    Create MulAndDiv.java

---
+++ MulAndDiv.java
@@ -0,0 +1,14 @@
+ import java.io.*;
+ import java.util.*;
+  public class MulAndDiv{
+    public static void main(String args[]){
+      Scanner sca=new Scanner(System.in);
+      System.out.println("A Value:");
+      int a=sca.nextInt();
+      System.out.println("B Value:");
+      int b=sca.nextInt();
+      System.out.println("C Value:");
+      int c=sca.nextInt();
+      System.out.println("(A*B)/c="+((a*b)/c));
+      }
+    }  

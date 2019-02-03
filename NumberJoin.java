4587f65fb8e2c15033939d6a0720ea6ae19b1854
subayadhav <suba10499@gmail.com>
2019-02-03 21:41:52 +0530

    Create NumberJoin.java

---
+++ NumberJoin.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+  public class NumberJoin{
+    public static void main(String args[]){
+      Scanner s=new Scanner(System.in);
+      System.out.println("Enter Num1:");
+      String num1=s.nextLine();
+      System.out.println("Enter Num1:");
+      String num2=s.nextLine();
+      System.out.println("-->"+(num1+num2));
+      }
+} 
